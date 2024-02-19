74bc09c950cbb5a44629c46203ced78c4c6376c0
Maxence Bombeeck <115149634+MaxenceB59@users.noreply.github.com>
2024-02-19 14:17:21 +0100

    [mbk][feats][FixingSidebar/ResolvingBugs]

--- src/main/java/com/istore/gui/controllers/dashboard/stores/StoreDetailsViewController.java
+++ src/main/java/com/istore/gui/controllers/dashboard/stores/StoreDetailsViewController.java
@@ -3,8 +3,16 @@ package com.istore.gui.controllers.dashboard.stores;
 import com.istore.gui.controllers.dashboard.DashboardController;
 import com.istore.models.Store;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+
+import static com.istore.Application.getAuthService;
 
 public class StoreDetailsViewController {
 
@@ -21,7 +29,27 @@ public class StoreDetailsViewController {
     @FXML
     private TableColumn<Store, Void> actionsColumn;
 
+    @FXML
+    private Button inventoryButton, employeesButton;
+
+    @FXML
+    private Button inventoryAddButton, employeesAddButton;
+
+    @FXML
+    private VBox inventorySection, employeesSection;
+
+    @FXML
+    private HBox horizontalBox;
+
     private DashboardController dashboardController;
+
+    /**
+     * Initialisation de la vue.
+     * @throws IOException Exception qui gère les erreurs de changement de vue
+     */
+    public void initialize() throws IOException {
+        this.handleInventorySection();
+    }
 
     public void setDashboardController(DashboardController dashboardController) {
         this.dashboardController = dashboardController;
@@ -32,5 +60,53 @@ public class StoreDetailsViewController {
         if (dashboardController != null) {
             dashboardController.loadStoresView();
         }
+    }
+
+    /**
+     * Permet d'afficher la section d'information et de masquer les autres sections.
+     */
+    @FXML
+    public void handleInventorySection() {
+        this.inventoryButton.setTextFill(Color.web("#007AFF"));
+        this.employeesButton.setTextFill(Color.web("#818181"));
+
+        this.inventoryAddButton.setVisible(true);
+        this.inventoryAddButton.setManaged(true);
+
+        this.employeesAddButton.setVisible(false);
+        this.employeesAddButton.setManaged(false);
+
+        this.employeesSection.setVisible(false);
+        this.employeesSection.setManaged(false);
+
+        this.inventorySection.setVisible(true);
+        this.inventorySection.setManaged(true);
+
+        this.horizontalBox.setSpacing(780);
+    }
+
+    /**
+     * Permet d'afficher la section d'information et de masquer les autres sections.
+     */
+    @FXML
+    public void handleEmployeesSection() {
+        this.employeesButton.setTextFill(Color.web("#007AFF"));
+        this.inventoryButton.setTextFill(Color.web("#818181"));
+
+
+        this.inventoryAddButton.setVisible(false);
+        this.inventoryAddButton.setManaged(false);
+
+        this.employeesAddButton.setVisible(true);
+        this.employeesAddButton.setManaged(true);
+
+        this.employeesAddButton.setVisible(true);
+        this.employeesAddButton.setManaged(true);
+
+        this.inventorySection.setVisible(false);
+        this.inventorySection.setManaged(false);
+
+        this.horizontalBox.setSpacing(755);
+
     }
 }
